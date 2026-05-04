6f8961959be31a23b29e2c0da7ae5b5db317559d
Usama Hafeez <usamahafeez096@gmail.com>
2026-05-04 17:22:26 +0500

    .

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -127,7 +127,7 @@ class _AppHeader extends StatelessWidget {
               ),
               const SizedBox(width: 10),
               Text(
-                'SALAH',
+                'Salah',
                 style: TextStyle(
                   color: t.accent,
                   fontSize: 20,
